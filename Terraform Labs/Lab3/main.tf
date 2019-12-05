ab788425a7e1238ebf33e4d50a2146fbf9cc0a67
vmkube <58411897+vmkube@users.noreply.github.com>
2019-12-05 11:31:40 -0800

    Add files via upload

--- Terraform Labs/Lab3/main.tf
+++ Terraform Labs/Lab3/main.tf
@@ -1,1 +1,86 @@
+# Connecting to vCD
+provider "vcd" {
+  user                  = var.vcd_user
+  password              = var.vcd_pass
+  org                   = var.vcd_org
+  url                   = var.vcd_url
+}
+
+# Create new org
+
+resource "vcd_org" "org-name" {
+  name             = var.org_name
+  full_name        = var.org_full_name
+  is_enabled       = "true"
+  delete_recursive = "true"
+  delete_force     = "true"
+  deployed_vm_quota = "10"
+  can_publish_catalogs = "false"
+}
+
+#Create new VDC
+
+resource "vcd_org_vdc" "my-vdc" {
+  name        = var.vdc_name
+  org         = var.org_name
+
+  allocation_model = "ReservationPool"
+  network_pool_name = "PVDC-A01-VXLAN-NP"
+  provider_vdc_name = "PVDC-A01"
+
+
+  compute_capacity {
+    cpu {
+      allocated = 2048
+    }
+
+    memory {
+      allocated = 2048
+    }
+  }
+
+  storage_profile {
+    name     = "Gold Tier Policy"
+    limit    = 10240
+    default  = true    
+  }
+
+  metadata = {
+    role    = "customerName"
+    env     = "staging"
+    version = "v1"
+  }  
+
+  network_quota = 10
+  enabled                  = true
+  enable_thin_provisioning = true
+  enable_fast_provisioning = true
+  delete_force             = true
+  delete_recursive         = true
+}
+
+# Create Edge GW
+
+resource "vcd_edgegateway" "egw" {
+org = var.org_name
+vdc = var.vdc_name
+
+  name                    = "terraform EGW"
+  description             = "new edge gateway"
+  configuration           = "compact"
+  default_gateway_network = "Site-A-ExtNet"
+  external_networks       = ["Site-A-ExtNet"] 
+  advanced                = true
+}
+
+# Create Org Network
+resource "vcd_network_routed" "net" {
+ org = var.org_name
+  vdc = var.vdc_name
+  ??
+}
+
+ # Create vApp - Servers ??
+ 
+
 
---
+++ Terraform Labs/Lab3/terraform.tfvars
@@ -0,0 +1,12 @@
+# vCD Parameters
+
+vcd_user = "terraform"
+vcd_pass = "VMware1!"
+vcd_url = "https://vcd-01a.corp.local/api"
+vcd_org = "System"
+org_name = "Terraform"
+org_full_name = "My Terraform Organization"
+vdc_name = "Terraform_vdc"
+
+
+
