4581f0efa30a068826ad9ca9e0b9228cf59b107d
vmkube <58411897+vmkube@users.noreply.github.com>
2019-12-05 11:30:46 -0800

    Add files via upload

--- Terraform Labs/Lab1/main.tf
+++ Terraform Labs/Lab1/main.tf
@@ -1,1 +1,20 @@
+# Connecting to vCD
+provider "vcd" {
+  user                  = "??"
+  password              = "??"
+  org                   = "System"
+  url                   = "https://vcd/api"
+}
+# Create new org
+
+resource "vcd_org" "org-name" {
+  name             = "??"
+  full_name        = "??"
+  is_enabled       = "true"
+  delete_recursive = "true"
+  delete_force     = "true"#  
+  deployed_vm_quota = "10"
+  can_publish_catalogs = "false"
+}
+
 
