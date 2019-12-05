83c378045512b7685729f2e1cdb109435169cb0c
vmkube <58411897+vmkube@users.noreply.github.com>
2019-12-05 11:31:13 -0800

    Add files via upload

--- Terraform Labs/Lab2/main.tf
+++ Terraform Labs/Lab2/main.tf
@@ -1,1 +1,69 @@
+# Connecting to vCD
+provider "vcd" {
+  user                  = var.vcd_user
+  password              = var.vcd_pass
+  org                   = var.vcd_org
+  url                   = var.vcd_url
+}
+
+# Create new org
+
+resource "vcd_org" "org-name" {
+  name             = var.org_name
+  full_name        = var.org_full_name
+  is_enabled       = "true"
+  delete_recursive = "true"
+  delete_force     = "true"
+  deployed_vm_quota = "10"
+  can_publish_catalogs = "false"
+}
+
+#Create new VDC
+
+resource "vcd_org_vdc" "my-vdc" {
+  name        = var.vdc_name
+  org         = var.org_name
+
+  allocation_model = "??"
+  network_pool_name = "??"
+  provider_vdc_name = "??"
+
+
+  compute_capacity {
+    cpu {
+      allocated = 2048
+    }
+
+    memory {
+      allocated = 2048
+    }
+  }
+
+  ??
+
+  metadata = {
+    role    = "customerName"
+    env     = "staging"
+    version = "v1"
+  }  
+
+  network_quota = 10
+  enabled                  = true
+  enable_thin_provisioning = true
+  enable_fast_provisioning = true
+  delete_force             = true
+  delete_recursive         = true
+}
+
+# Create Edge GW
+
+resource "vcd_edgegateway" "egw" {
+org = var.org_name
+vdc = var.vdc_name
+
+ ??
+}
+
+
+
 
---
+++ Terraform Labs/Lab2/variables.tf
@@ -0,0 +1,28 @@
+# vCD Variables
+variable "vcd_user" {
+    description = "vCloud user"
+}
+
+variable "vcd_pass" {
+    description = "vCloud pass"
+}
+
+variable "vcd_url" {
+    description = "vCloud url"
+}
+
+variable "vcd_org" {
+    description = "vCloud org"
+}
+
+variable "org_name" {
+    description = "vCloud org name"
+}
+
+variable "org_full_name" {
+    description = "vCloud org full name"
+}
+
+variable "vdc_name" {
+    description = "VDC Name"
+}
